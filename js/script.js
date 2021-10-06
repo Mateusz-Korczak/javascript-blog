4414ddab0bf4f7e36cb45e8d39d53b244ee8196e
Mateusz Korczak <korczak.programowanie@gmail.com>
2021-10-06 18:12:03 +0200

    for me commit with question and comments

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+
 const titleClickHandler = function(event){
     event.preventDefault();
     const clickedElement = this;
@@ -11,6 +12,8 @@ const titleClickHandler = function(event){
     }
 
     /* [DONE] add class 'active' to the clicked link */
+    // console.log('clickedElement:', clickedElement);
+    // console.log('clickedElement (with plus): ' + clickedElement);
     clickedElement.classList.add('active');
 
     /* [DONE] remove class 'active' from all articles */
@@ -20,16 +23,20 @@ const titleClickHandler = function(event){
     }
 
     /* [DONE] get 'href' attribute from the clicked link */
+    // const hrefer = clickedElement.href;
+    // console.log("to ten href: " + hrefer);
     const clickedElementHref = clickedElement.getAttribute("href");
     console.log("getElement href: " + clickedElementHref);
+    // Podświetlenia rozwiązane za pomocą CSS
 
     /* [DONE] find the correct article using the selector (value of 'href' attribute) */
     const selectedArticle = document.body.querySelector(clickedElementHref);
-
+    // [PYTANIE] jak zrobić selector z wyszukanim id, ktore jest zawarte w zmiennej???
+    // document.querySelector(".post [id=`${clickedElementHref}`]"); tutaj nie działa `${xyz}`
+    
     /* [DONE] add class 'active' to the correct article */
     selectedArticle.classList.add('active');
 }
-
 const links = document.querySelectorAll('.titles a');
   
 for(let link of links){
